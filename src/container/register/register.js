019a412b085d74b53092a82351c69f0bf98d29a4
liuchenghu <786973980@qq.com>
2019-07-24 16:56:20 +0800

    注册交互实现

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -6,28 +6,49 @@ const RadioItem = Radio.RadioItem;
 class Register extends Component {
     constructor(props) {
         super(props)
+        this.handleRegister = this.handleRegister.bind(this);
         this.state = {
+            user: '',
+            pwd: '',
+            repeatpwd: '',
             type: 'genius'
         }
     }
+
+    handleChange(key, val) {
+        this.setState({
+            [key]: val    // key要加[],不然就是字符串,这种语法是ES6的属性名表达式
+        })
+    }
+
+    handleRegister() {
+        console.log(this.state)
+    }
     render() {
+        const { type } = this.state;
         return (
             <div>
                 <Logo />
                 <WingBlank>
                     <List>
-                        <InputItem>用户名</InputItem>
-                        <InputItem>密码</InputItem>
-                        <InputItem>确认密码</InputItem>
-                        <RadioItem checked={this.state.type === 'genius'}>
+                        <InputItem onChange={v => this.handleChange('user', v)}>用户名</InputItem>
+                        <InputItem type="password" onChange={v => this.handleChange('pwd', v)}>密码</InputItem>
+                        <InputItem type="password" onChange={v => this.handleChange('repeatpwd', v)}>确认密码</InputItem>
+                        <RadioItem 
+                            checked={type === 'genius'}
+                            onChange={() => this.handleChange('type','genius')}
+                        >
                             牛人
                         </RadioItem>
-                        <RadioItem checked={this.state.type === 'boss'}>
+                        <RadioItem 
+                            checked={type === 'boss'}
+                            onChange={() => this.handleChange('type','boss')}
+                        >
                             Boss
                         </RadioItem>
                     </List>
                     <WhiteSpace />
-                    <Button type="primary">注册</Button>
+                    <Button type="primary" onClick={this.handleRegister}>注册</Button>
                 </WingBlank>
             </div>
         )
